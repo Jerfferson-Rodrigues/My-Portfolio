0e21958f97e33e9ba0e6a2746f3744a1b3d72d41
Jerfferson-Rodrigues <brjerffersonrodriues@outlook.com>
2021-09-15 23:53:55 -0300

    animation

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function toggleMenu() {
     menutoggle.classList.toggle('active')
     menu.classList.toggle('active')
 }
+
+/* ==== GSAP ANIMATION ====*/
+/// header
+gsap.from('.logo, .toggle', { opacity: 0, duration: 1, delay: 2, y: 10 })
+gsap.from('.item', { opacity: 0, duration: 1, delay: 2.1, x: 45, stagger: 0.2 })
+
+// HOME
+gsap.from('.textBn', { opacity: 0, duration: 1, delay: 1.6, y: 45 })
+gsap.from('.btn', { opacity: 0, duration: 1, delay: 2.1, y: 45 })
